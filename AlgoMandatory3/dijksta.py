50915605d25c9313a974eb4126cd07a602e50d49
Erdvik97 <magnuserdvik@hotmail.com>
2020-04-01 14:02:15 +0200

    added comments to the code

--- AlgoMandatory3/dijksta.py
+++ AlgoMandatory3/dijksta.py
@@ -1,6 +1,9 @@
 from heapq import heappop, heappush
 from math import inf
 
+#auth: Magnus and Kantasit
+
+#The graph that is used in the task description, using a python dictionary
 graph = { 
         'A': [('B', 10), ('C', 5), ('D', 9999), ('E', 3), ('F', 12)],
         'B': [('C', 17), ('D', 9), ('E', 17), ('F', 12), ('A', 10)],
@@ -10,27 +13,27 @@ graph = {
         'F': [('A', 12), ('B', 12), ('C', 12), ('D', 12), ('E', 12)]
     }
 
-
-def dijkstras(graph, start):
-  distances = {}
+#the implemenmtation of the dijkstra algorithm 
+def dijkstras(graph, start): #the method takes in the graph and a start vertex
+  distances = {} #a dictionary to hold the distances between the vertices
   
-  for vertex in graph:
-    distances[vertex] = inf
-    
-  distances[start] = 0
-  vertices_to_explore = [(0, start)]
+  for vertex in graph: 
+    distances[vertex] = inf #sets the distances from the start vertex to all other vertices to infinity, since they are not yet known  
   
-  while vertices_to_explore:
-    current_distance, current_vertex = heappop(vertices_to_explore)
+  distances[start] = 0 #sets the distance to the start vertex to 0 since it is the same vertex 
+  vertices_to_explore = [(0, start)] #initializes a heap of vertices to explore 
+  
+  while vertices_to_explore: #loop that runs as long as there are still verticies not explored
+    current_distance, current_vertex = heappop(vertices_to_explore) #heappop() returns the smallest item from vertexes_to_explore heap
     
-    for neighbor, edge_weight in graph[current_vertex]:
+    for neighbor, edge_weight in graph[current_vertex]: #checks all the neighboring vertices and distances (edge_weight)
       new_distance = current_distance + edge_weight
          
-      if new_distance < distances[neighbor]:
+      if new_distance < distances[neighbor]: #if the new distance is smaller than the distance from the other neighbors, the distance is updated with the new distance
         distances[neighbor] = new_distance
-        heappush(vertices_to_explore, (new_distance, neighbor))
+        heappush(vertices_to_explore, (new_distance, neighbor)) #uses heappush() to push the new vertex to the vertices_to_explore heap
      
-  return distances
+  return distances #returns the dictionary of shortest distances to the vertices in the graph from the start vertex
 
 print("Graph:")
 print("")
@@ -38,9 +41,11 @@ print(graph)
 print("")
 print("Cheapest distance to connect all locations using Dijkstra's algorithm: ")
 print("")
-for main_vertex in graph:
-  distances = dijkstras(graph, main_vertex)
+
+#a loop that runs the dijkstras() method on all the vertecies in the graph, answering problem 1 and 2
+for start_vertex in graph:
+  distances = dijkstras(graph, start_vertex)
   for vertex in distances:
-    print("[" + str(main_vertex) + str(vertex) + "] = " + str(distances.get(vertex)))
+    print("[" + str(start_vertex) + str(vertex) + "] = " + str(distances.get(vertex)))
   
   
